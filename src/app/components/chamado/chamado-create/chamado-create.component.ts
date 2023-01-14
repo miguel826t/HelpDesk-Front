72e2893a5885e7c24668b15a264fecee4438fced
miguel826t <miguel826t@gmail.com>
2023-01-14 17:39:32 -0300

    Adicionado validação ao campos do criar chamado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  titulo     : FormControl = new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(30)])
+  status     : FormControl = new FormControl(null,[Validators.required])
+  prioridade : FormControl = new FormControl(null,[Validators.required])
+  tecnico    : FormControl = new FormControl(null,[Validators.required])
+  cliente    : FormControl = new FormControl(null,[Validators.required])
+  descricao  : FormControl = new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(30)])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean{
+    return this.titulo.valid && this.status.valid &&
+           this.prioridade.valid && this.tecnico.valid &&
+           this.cliente.valid && this.descricao.valid
   }
 
 }
